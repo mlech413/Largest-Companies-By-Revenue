94012cb3487a7f06f8d1310cc3f6af18380e01c3
mlech413 <mlech413@gmail.com>
2023-04-20 11:36:20 -0400

    third chart finished

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ from config import user, password, host, port, db
 
 app = Flask(__name__)
 
+# Credentials from the hidden config file
 engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}")
 
+# Route for rendering the initial landing page
 @app.route("/")
 def home():
 
     return render_template("index.html")
 
+# Route for rendering the linked dashboard page
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
+# API to retrieve a full list of company info from the companies table
 @app.route("/api/v1.0/companies")
 def companies():
     conn = engine.connect()
@@ -23,27 +27,23 @@ def companies():
     df1 = pd.read_sql(query, conn)
     return df1.to_json(orient="records")
 
+# API to retrieve stock names and associated symbols for populating dropdown list
 @app.route("/api/v1.0/names")
 def names():
     conn = engine.connect()
-    query = "SELECT name, symbol, industry FROM companies WHERE symbol IS NOT NULL ORDER BY name"
+    query = "SELECT name, symbol FROM companies WHERE symbol IS NOT NULL ORDER BY name"
     nameList = pd.read_sql(query, conn)
     return nameList.to_json(orient="records")
 
+# API to retrieve industry and revenue data
 @app.route("/api/v1.0/industries")
 def industries():
     conn = engine.connect()
-    query = "SELECT industry, rev_usd_mil FROM companies ORDER BY 1"
+    query = "SELECT industry, name, rev_usd_mil FROM companies ORDER BY industry"
     indList = pd.read_sql(query, conn)
     return indList.to_json(orient="records")
 
-# @app.route("/api/v1.0/industries")
-# def industries():
-#     conn = engine.connect()
-#     query = "SELECT industry, MAX(rev_usd_mil) as rev_usd_mil, min(rank) as rank FROM companies GROUP BY industry ORDER BY rev_usd_mil DESC"
-#     indList = pd.read_sql(query, conn)
-#     return indList.to_json(orient="records")
-
+# API to retrieve data for the stock that the user selected in the dropdown
 @app.route("/api/v1.0/<stock>")
 def stocks(stock):
     conn = engine.connect()
